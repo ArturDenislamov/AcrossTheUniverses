51ea2128b9b3db12fb134af59e4bbdb47339a6c8
homeofrock <38583574+homeofrock@users.noreply.github.com>
2018-12-15 18:14:51 +0500

    first commit

---
+++ PractGame/core/src/com/practgame/game/PractGame.java
@@ -0,0 +1,24 @@
+package com.practgame.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.practgame.game.Screens.PlayScreen;
+import com.practgame.game.Screens.TitleScreen;
+
+public class PractGame extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	public SpriteBatch batch;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+}
